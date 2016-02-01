b77ec22a5884ad194497d2d362a9afce81de7631
Ethan <erdesi90562@gmail.com>
2016-02-01 13:35:55 -0500

    lesson complete

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,12 +1,16 @@
 'use strict';
 
+const myApp = {
+  baseUrl: 'http://10.13.105.91:3000/'
+}
+
 $(document).ready(() => {
   $('#sign-up').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(e.target);
     $.ajax({
-      // url: 'http://localhost:3000/sign-up',
-      url: 'http://httpbin.org/post',
+      url: myApp.baseUrl + 'sign-up',
+      //url: 'http://httpbin.org/post',
       method: 'POST',
       contentType: false,
       processData: false,
@@ -17,6 +21,53 @@ $(document).ready(() => {
       console.error(jqxhr);
     });
   });
+// super dirty sign in code
+  $('#sign-in').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.baseUrl + 'sign-in',
+      method: 'POST',
+      contentType: false,
+      processData: false,
+      data: formData,
+    }).done(function(data) {
+      myApp.user = data.user;
+      console.log(data.user);
+      console.log(data);
+      console.log(myApp);
+    }).fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+
+  $('#change-password').on('submit', function(e) {
+    e.preventDefault();
+    if (!myApp.user) {
+      console.error('shit not logged in.');
+      return;
+    }
+
+    var formData = new FormData(e.target);
+    $.ajax({
+      url: myApp.baseUrl + 'change-password/' + myApp.user.id,
+      method: 'PATCH',
+      headers: {
+        Authorization: 'Token token=' + myApp.user.token,
+      },
+      contentType: false,
+      processData: false,
+      data: formData,
+    })
+    .done(function(data) {
+      console.log(data);
+      console.log(myApp);
+    })
+    .fail(function(jqxhr) {
+      console.error(jqxhr);
+    });
+  });
+
 });
 
 module.exports = true;
